f256ef6846fcb4cd1d88a32769e27f54da42866d
Sakib Hasan <sakib100.sa@gmail.com>
2022-10-02 00:31:38 +0600

    some bug fixed and added song title on the bottom bar

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Initialize the variables
-let songIndex = 0;
-let audioElement = new Audio("./songs/1.mp3");
+let songIndex = 1;
+let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
 let songItems = Array.from(document.getElementsByClassName("songItem"));
+let songItemPlay = Array.from(document.getElementsByClassName("songItemPlay"));
+let songTitle = document.getElementById("songTitle");
+console.log(songTitle);
 let songs = [
 	{
 		songName: "Let me Love You - DJ Snake",
@@ -57,7 +60,7 @@ let songs = [
 		cover: "covers/10.jpg",
 	},
 ];
-
+// Handle DOM input
 songItems.forEach((element, i) => {
 	element.getElementsByTagName("img")[0].src = songs[i].cover;
 	element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
@@ -70,6 +73,7 @@ masterPlay.addEventListener("click", () => {
 		masterPlay.classList.remove("fa-play-circle");
 		masterPlay.classList.add("fa-pause-circle");
 		gif.style.opacity = "1";
+		songTitle.innerText = songs[songIndex - 1].songName;
 	} else {
 		audioElement.pause();
 		masterPlay.classList.remove("fa-pause-circle");
@@ -90,4 +94,44 @@ audioElement.addEventListener("timeupdate", () => {
 myProgressBar.addEventListener("change", () => {
 	audioElement.currentTime =
 		(myProgressBar.value * audioElement.duration) / 100;
+});
+const makeAllPlays = () => {
+	songItemPlay.forEach((element) => {
+		element.classList.remove("fa-pause-circle");
+		element.classList.add("fa-play-circle");
+	});
+};
+
+songItemPlay.forEach((element) => {
+	element.addEventListener("click", (e) => {
+		makeAllPlays();
+		songIndex = parseInt(e.target.id);
+		e.target.classList.remove("fa-play-circle");
+		e.target.classList.add("fa-pause-circle");
+		audioElement.src = `songs/${songIndex}.mp3`;
+		audioElement.currentTime = 0;
+		audioElement.play();
+		masterPlay.classList.remove("fa-play-circle");
+		masterPlay.classList.add("fa-pause-circle");
+		songTitle.innerText = songs[songIndex - 1].songName;
+		gif.style.opacity = "1";
+	});
+});
+
+// Handle next/previous click
+
+document.getElementById("next").addEventListener("click", () => {
+	if (songIndex >= 10) {
+		songIndex = 1;
+	} else {
+		songIndex++;
+	}
+	console.log(songIndex);
+	audioElement.src = `songs/${songIndex}.mp3`;
+	audioElement.currentTime = 0;
+	audioElement.play();
+	masterPlay.classList.remove("fa-play-circle");
+	masterPlay.classList.add("fa-pause-circle");
+	songTitle.innerText = songs[songIndex - 1].songName;
+	gif.style.opacity = "1";
 });
