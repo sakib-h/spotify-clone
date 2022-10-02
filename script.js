aefece4c157c55628276c8137eec1512d1ddd7db
Sakib Hasan <sakib100.sa@gmail.com>
2022-10-02 17:52:22 +0600

    some error fix

--- script.js
+++ script.js
@@ -7,63 +7,74 @@ let gif = document.getElementById("gif");
 let songItems = Array.from(document.getElementsByClassName("songItem"));
 let songItemPlay = Array.from(document.getElementsByClassName("songItemPlay"));
 let songTitle = document.getElementById("songTitle");
-console.log(songTitle);
+let songDuration = document.getElementById("songDuration");
 let songs = [
 	{
 		songName: "Let me Love You - DJ Snake",
 		filePath: "songs/1.mp3",
 		cover: "covers/1.jpg",
+		duration: "3:25",
 	},
 	{
 		songName: "Unstoppable - Sia",
 		filePath: "songs/2.mp3",
 		cover: "covers/2.jpg",
+		duration: "3:46",
 	},
 	{
 		songName: "Girls Like You - Maroon 5",
 		filePath: "songs/3.mp3",
 		cover: "covers/3.jpg",
+		duration: "4:30",
 	},
 	{
 		songName: "Perfect - Ed Sheeran",
 		filePath: "songs/4.mp3",
 		cover: "covers/4.jpg",
+		duration: "4:39",
 	},
 	{
 		songName: "Memories - Maroon 5",
 		filePath: "songs/5.mp3",
 		cover: "covers/5.jpg",
+		duration: "3:15",
 	},
 	{
 		songName: "Cheap Thriills - Sia",
 		filePath: "songs/6.mp3",
 		cover: "covers/6.jpg",
+		duration: "4:21",
 	},
 	{
 		songName: "Shape of You - Ed Sheeran",
 		filePath: "songs/7.mp3",
 		cover: "covers/7.jpg",
+		duration: "4:23",
 	},
 	{
 		songName: "Love me like you do - Ellie Goulding",
 		filePath: "songs/8.mp3",
 		cover: "covers/8.jpg",
+		duration: "4:09",
 	},
 	{
 		songName: "We don't talk anymore - Charlie Puth",
 		filePath: "songs/9.mp3",
 		cover: "covers/9.jpg",
+		duration: "3:50",
 	},
 	{
 		songName: "Let me down slowly - Alec Benjamin",
 		filePath: "songs/10.mp3",
 		cover: "covers/10.jpg",
+		duration: "2:57",
 	},
 ];
 // Handle DOM input
 songItems.forEach((element, i) => {
 	element.getElementsByTagName("img")[0].src = songs[i].cover;
 	element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+	element.getElementsByClassName("duration")[0].innerText = songs[i].duration;
 });
 
 // Handle play/pause click
@@ -74,6 +85,7 @@ masterPlay.addEventListener("click", () => {
 		masterPlay.classList.add("fa-pause-circle");
 		gif.style.opacity = "1";
 		songTitle.innerText = songs[songIndex - 1].songName;
+		songDuration.innerText = songs[songIndex - 1].duration;
 	} else {
 		audioElement.pause();
 		masterPlay.classList.remove("fa-pause-circle");
@@ -115,6 +127,7 @@ songItemPlay.forEach((element) => {
 		masterPlay.classList.add("fa-pause-circle");
 		songTitle.innerText = songs[songIndex - 1].songName;
 		gif.style.opacity = "1";
+		songDuration.innerText = songs[songIndex - 1].duration;
 	});
 });
 
@@ -126,7 +139,6 @@ document.getElementById("next").addEventListener("click", () => {
 	} else {
 		songIndex++;
 	}
-	console.log(songIndex);
 	audioElement.src = `songs/${songIndex}.mp3`;
 	audioElement.currentTime = 0;
 	audioElement.play();
@@ -134,4 +146,21 @@ document.getElementById("next").addEventListener("click", () => {
 	masterPlay.classList.add("fa-pause-circle");
 	songTitle.innerText = songs[songIndex - 1].songName;
 	gif.style.opacity = "1";
+	songDuration.innerText = songs[songIndex - 1].duration;
+});
+
+document.getElementById("previous").addEventListener("click", () => {
+	if (songIndex <= 1) {
+		songIndex = 10;
+	} else {
+		songIndex--;
+	}
+	audioElement.src = `songs/${songIndex}.mp3`;
+	audioElement.currentTime = 0;
+	audioElement.play();
+	masterPlay.classList.remove("fa-play-circle");
+	masterPlay.classList.add("fa-pause-circle");
+	songTitle.innerText = songs[songIndex - 1].songName;
+	gif.style.opacity = "1";
+	songDuration.innerText = songs[songIndex - 1].duration;
 });
